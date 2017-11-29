3fe91828c9f50814fdb5380bf9a2e90f0a40e6f7
Minsu <minsu.lee.it@gmail.com>
2017-11-29 00:53:37 -0400

    test base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://ec2-35-182-196-72.ca-central-1.compute.amazonaws.com/CodeIgniter/';
 
 /*
 |--------------------------------------------------------------------------
